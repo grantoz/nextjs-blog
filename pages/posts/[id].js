af7759be7cb87bacd6f6c1bd65ba2fd4aeeb0066
Grant Ozolins <grant.ozolins@gmail.com>
2020-12-11 10:33:26 +1100

    add 404 page, remove multiline object definitions where simple

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -6,19 +6,12 @@ import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
-  return {
-    props: {
-      postData
-    }
-  }
+  return { props: { postData }}
 }
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
-  return {
-    paths,
-    fallback: false
-  }
+  return { paths, fallback: false }
 }
 
 export default function Post({ postData }) {
